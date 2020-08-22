c1c350fec9536a679c2f3a84934fffae38a3ac4a
Austin <aakerley@tamu.edu>
2020-08-21 21:04:52 -0600

    needed to remove spaces

--- raspberry_pi_timelapse.py
+++ raspberry_pi_timelapse.py
@@ -49,6 +49,7 @@ id = 1
 while (start_time-current_time).total_seconds() < time_running_to_sec:
     current_time = datetime.datetime.now()
     photo_name = "./garden" + str(current_time)+".jpg"
+    photo_name = photo_name.strip()
     os.system("raspistill -o "+photo_name)
     time.sleep(10)
     image = convert_image_to_blob(photo_name)
